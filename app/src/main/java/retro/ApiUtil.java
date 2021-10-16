81a7686415cc3bd03daada50d30431b6fd2a0acb
Pongsametrey Sok <metreysk@gmail.com>
2021-10-16 13:10:11 +0700

    adaptation for recent update

--- app/src/main/java/retro/ApiUtil.java
+++ app/src/main/java/retro/ApiUtil.java
@@ -2,7 +2,7 @@ package retro;
 
 public class ApiUtil {
 
-    private static final String BASE_URL = "http://ask.osify.com/wp-json/wp/";
+    private static final String BASE_URL = "https://ask.osify.com/wp-json/wp/";
 
     public static RetrofitInterface getServiceClass(){
         return RetrofitAPI.getRetrofit(BASE_URL).create(RetrofitInterface.class);
